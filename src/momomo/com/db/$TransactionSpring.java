826821de82e82411c111f00ab40694dcaac7e142
momomo <opensource@momomo.com>
2024-11-22 15:18:58 +0100

    Committing Fri Nov 22 15:18:58 CET 2024

---
+++ src/momomo/com/db/$TransactionSpring.java
@@ -0,0 +1,34 @@
+package momomo.com.db;
+
+import org.springframework.transaction.TransactionStatus;
+
+public final class $TransactionSpring extends $Transaction<$TransactionSpring> {
+    public final $TransactionManagerSpring manager;
+    public final TransactionStatus         delegate;
+
+    protected $TransactionSpring($TransactionManagerSpring outer, TransactionStatus delegate, boolean isNew) {
+        super(outer, isNew ? null : Boolean.FALSE );
+    
+        this.manager  = outer;
+        this.delegate = delegate;   
+    }
+    
+    /**
+     * To grant access to the underlying transaction should there be a need
+     */
+    public TransactionStatus delegate() {
+        return delegate; 
+    }
+    
+    /**
+     * To grant access to our 'transaction manager' from a transaction if there is ever a need
+     */
+    public $TransactionManagerSpring manager() {
+        return manager; 
+    }
+    
+    @Override
+    public String toString() {
+        return getClass().getName() + "@" + System.identityHashCode(delegate);
+    }
+}
